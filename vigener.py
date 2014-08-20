89c7ea44a64a78094fc4bee99cdc1d9917864b62
Tavi Nathanson <tavi.nathanson@gmail.com>
2014-08-20 16:34:07 -0400

    Adding Vigener decryption, and a .gitignore

--- vigener.py
+++ vigener.py
@@ -6,19 +6,38 @@ import math
 
 def encrypt(message, key):
     new_letters = []
-    if len(key) < len(message):
-        key = key * int(math.ceil(len(message) / float(len(key))))
+    key = expand_key(key, len(message))
     for message_letter, key_letter in zip(message, key):
         new_num = (to_num(message_letter) + to_num(key_letter)) % 26
         new_letter = to_letter(new_num)
         new_letters.append(new_letter)
     return ''.join(new_letters)
 
+def decrypt(cipher, key):
+    new_letters = []
+    key = expand_key(key, len(cipher))
+    for cipher_letter, key_letter in zip(cipher, key):
+        new_num = (to_num(cipher_letter) - to_num(key_letter)) % 26
+        new_letter = to_letter(new_num)
+        new_letters.append(new_letter)
+    return ''.join(new_letters)
+
+def expand_key(key, length):
+    if len(key) < length:
+        return key * int(math.ceil(length / float(len(key))))
+    return key
+
 def to_num(letter):
-    return ord(letter.capitalize()) - ord('A')
+    return ord(letter.capitalize()) - (ord('A') - 1)
 
 def to_letter(num):
-    return chr(num + ord('A') + 1)
+    # TODO Fix this hack
+    if num == 0:
+        return 'Z'
+    return chr(num + ord('A') - 1)
 
-if __name__ == "__main__":    
-    print(encrypt("WHATANICEDAYTODAY", "CRYPTO"))
+if __name__ == "__main__":
+    cipher = encrypt("WHATANICEDAYTODAY", "CRYPTO")
+    print("Cipher: %s" % cipher)
+    decrypted = decrypt(cipher, "CRYPTO")
+    print("Decrypted: %s" % decrypted)
